978d8de1221adda09cd2ad2f988327ada2b95455
Cameron Reese <cameronreese@mac.com>
2015-05-26 00:04:48 -0500

    removed no longer required variables and code that were previously just commented out

--- PE005/src/Main.java
+++ PE005/src/Main.java
@@ -8,28 +8,17 @@ What is the smallest positive number that is evenly divisible by all of the numb
 public class Main {
 
     public static void main(String[] args) {
-//        int multiples_of_20[] = {20, 10, 5, 4, 2, 1};
-//        int multiples_of_18[] = {18, 9, 6, 3, 2, 1};
-//        int multiples_of_16[] = {16, 8, 4, 2, 1};
-//        int multiples_of_15[] = {15, 5, 3, 1};
-//        int multiples_of_14[] = {14, 7, 2, 1};
-//        int multiples_of_12[] = {12, 6, 4, 3, 2, 1};
-//        int prime_numbers_less_than_twenty[] = {19, 17, 13, 11, 7, 3, 2};
 
-        //20 thru 11 contain all the multiples for 10 thru 1
-        //calculate 20 times 19 then check if any of the other numbers are multiples of the product, if they are not
-        boolean smallest_multiple_not_found = true;
         LinkedList<Long> multiples = new LinkedList<Long>();
         //populate a list with numbers from 20 to 11
         for (long i = 20; i >= 11; i--){
             multiples.add(i);
         }
 
-
         long current_product = multiples.getFirst();     //take care of multiples 20 and 1
         multiples.remove(0);                             //remove the multiple 20
 
-        long temp_product = 0;
+        long temp_product;
         while (multiples.size() > 0) {
 
             //iterate thru list and remove any multiple that is a multiple of the current product
@@ -44,11 +33,9 @@ public class Main {
                     }
                     temp_product = current_product;
                 }
-
             }
         }
         System.out.print(current_product);
     }
-
 }
 
